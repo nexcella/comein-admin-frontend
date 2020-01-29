0010905ee76b444a09446486e592d9a6002e97e7
Andrey Shabanov <a.shabanov.develop@gmail.com>
2020-01-29 11:43:59 +0300

    feat(version): init version provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {ReactNode} from "react";
 
 import logo from './logo.png';
 
+function VersionProvider({children}: { children: ReactNode }) {
+  const version = '0.0.1';
+  console.debug({version});
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
 export function App() {
   return (
-    <div>
+    <VersionProvider>
       App
       <div/>
       <img src={logo}/>
-    </div>
+    </VersionProvider>
 
   )
 }
