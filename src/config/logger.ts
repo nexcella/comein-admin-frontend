24d390c2d422b62f3308ffe3d1518d29875cb855
Andrey Shabanov <a.shabanov.develop@gmail.com>
2020-03-05 13:23:26 +0300

    chore(deps): use nexcella logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,6 +1,4 @@
-import {LOG_LEVEL, LoggerConfig} from "../utils/logger";
-
-
+import {LOG_LEVEL, LoggerConfig} from "@nexcella/logger";
 
 export const loggerConfig: LoggerConfig = {
   logLevel: process.env.LOG_LEVEL as unknown as LOG_LEVEL || LOG_LEVEL.DEBUG
