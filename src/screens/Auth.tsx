3394ae208c44588704205d8b4b4f99eec64a5905
Andrey Shabanov <a.shabanov.develop@gmail.com>
2020-03-13 19:42:09 +0300

    feat(validation): auth form

--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -8,22 +8,47 @@ import {Logo} from "../components/logo/Logo";
 import {AuthForm} from "../components/auth/AuthForm";
 
 const AuthWrapper = styled.div`
-  margin: 0 auto;
   display: flex;
   flex-direction: column;
   text-align: center;
-  width: 200px;
+  justify-content: space-between;
+  height: 100vh;
+`
+
+const FormWrapper = styled.div`
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
 `
 
 const Subtitle = styled.span`
   color: #5F5E5E;
-  margin-top: 8px;
+  margin: 8px auto;
   font-size: 16px;
+  width: 200px;
+`
+
+const ForgotLinkWrapper = styled.div`
+  text-align: right;
+  margin-top: 10px;
 `
 
 const ForgotLink = styled(Link)`
-
+  font-size: 14px;
+  color: #45A5F9;
+  text-decoration: none;
 `;
+
+const RegisterWrapper = styled.div`
+  padding-bottom: 24px;
+  color: #5F5E5E;
+`
+
+const RegisterLink = styled(Link)`
+  color: #45A5F9;
+  text-decoration: none;
+`
 
 
 export const Auth = () => {
@@ -34,12 +59,18 @@ export const Auth = () => {
 
   return (
     <AuthWrapper>
-      <Logo/>
-      <Subtitle>личный кабинет организатора</Subtitle>
-      <AuthForm/>
-      <div>
-        <Link to='/forgot'>забыли пароль?</Link>
-      </div>
+      <div/>
+      <FormWrapper>
+        <Logo/>
+        <Subtitle>личный кабинет организатора</Subtitle>
+        <AuthForm/>
+        <ForgotLinkWrapper>
+          <ForgotLink to='/forgot'>забыли пароль?</ForgotLink>
+        </ForgotLinkWrapper>
+      </FormWrapper>
+      <RegisterWrapper>
+        Нет аккаунта? <RegisterLink to={'/register'}>Создайте своё первое мероприятие</RegisterLink>
+      </RegisterWrapper>
     </AuthWrapper>
   );
 };
