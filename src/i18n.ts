58c737907a528092773ee08cf11af1ba62749529
Andrey Shabanov <a.shabanov.develop@gmail.com>
2020-03-13 19:42:09 +0300

    feat(validation): auth form

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,7 @@
 import i18n from 'i18next';
 import {initReactI18next} from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
+
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
@@ -11,24 +12,39 @@ i18n
     },
     resources: {
       'ru-RU': {
-        'translation': {
-          'button': {
+        translation: {
+          button: {
             login: 'Войти'
           },
           label: {
             'username': 'Логин',
             'password': 'Пароль'
+          },
+          validation: {
+            required: 'обязательное поле',
+            min: {
+              count_1: '{{field}} должен быть более {{count}} символа',
+              count_2: '{{field}} должен быть более {{count}} символов',
+              count_5: '{{field}} должен быть более {{count}} символов',
+            }
           }
         }
       },
       'en-EN': {
-        'translation': {
-          'button': {
+        translation: {
+          button: {
             login: 'Sign In'
           },
           label: {
-            'username': 'Username',
-            'password': 'Password'
+            username: 'Username',
+            password: 'Password'
+          },
+          validation: {
+            required: 'Required',
+            min: {
+              count: '{{field}} must be at least {{count}} character',
+              count_plural: '{{field}} must be at least {{count}} characters',
+            }
           }
         }
       }
--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -2,7 +2,7 @@ export class AuthService {
 
   login<T>(username: string, password: string): Promise<T> {
     return new Promise((resolve, reject) => {
-      setTimeout(() => resolve({token: '123123'} as unknown as T), 5000);
+      setTimeout(() => resolve({token: '123123'} as unknown as T), 25000);
     });
   }
 
