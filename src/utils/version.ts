8d65f8702902b46bbbbf1f81e840733df40ae369
Andrey Shabanov <a.shabanov.develop@gmail.com>
2020-06-16 04:54:00 +0300

    feat(ci): production build / 3

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,6 +1,6 @@
 export function getVersion() {
   const packageVersion = process.env.PKG_VERSION;
-  const appVersion = process.env.APP_VERSION || 'dev';
+  const appVersion = process.env.VERSION || 'dev';
 
   return `${packageVersion}-${appVersion}`;
 }
